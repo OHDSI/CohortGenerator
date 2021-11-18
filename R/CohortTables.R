b016dd5be170180b4e5194f7f358980f04606c24
Anthony Sena <asena5@its.jnj.com>
2021-11-18 14:43:12 -0500

    Revise cohort table creation to support incremental mode

--- R/CohortTables.R
+++ R/CohortTables.R
@@ -57,10 +57,7 @@ getCohortTableNames <- function(cohortTable = "cohort",
 #'
 #' @description
 #' This function creates an empty cohort table and empty tables for
-#' cohort statistics. NOTE: This function will drop the tables if they 
-#' already exist in your cohortDatabaseSchema. You can use 
-#' \code{\link[DatabaseConnector::getTableNames]{DatabaseConnector::getTableNames}}
-#' to check if these tables exist before calling this function.
+#' cohort statistics. 
 #'
 #' @template Connection
 #'
@@ -70,40 +67,71 @@ getCohortTableNames <- function(cohortTable = "cohort",
 #'
 #' @param cohortTableNames            The names of the cohort tables. See \code{\link{getCohortTableNames}}
 #'                                    for more details.
+#'                                    
+#' @param incremental                 When set to TRUE, this function will check to see
+#'                                    if the cohortTableNames exists in the cohortDatabaseSchema
+#'                                    and if they exist, it will skip creating the tables.
 #'
 #' @export
 createCohortTables <- function(connectionDetails = NULL,
                                connection = NULL,
                                cohortDatabaseSchema,
-                               cohortTableNames = getCohortTableNames()) {
+                               cohortTableNames = getCohortTableNames(),
+                               incremental = FALSE) {
   if (is.null(connection) && is.null(connectionDetails)) {
     stop("You must provide either a database connection or the connection details.")
   }
   
   start <- Sys.time()
-  ParallelLogger::logInfo("Creating cohort tables")
   if (is.null(connection)) {
     connection <- DatabaseConnector::connect(connectionDetails)
     on.exit(DatabaseConnector::disconnect(connection))
   }
-  sql <- SqlRender::readSql(system.file("sql/sql_server/CreateCohortTables.sql", package = "CohortGenerator", mustWork = TRUE))
-  sql <- SqlRender::render(sql = sql, 
-                           cohort_database_schema = cohortDatabaseSchema,
-                           cohort_table = cohortTableNames$cohortTable,
-                           cohort_inclusion_table = cohortTableNames$cohortInclusionTable,
-                           cohort_inclusion_result_table = cohortTableNames$cohortInclusionResultTable,
-                           cohort_inclusion_stats_table = cohortTableNames$cohortInclusionStatsTable,
-                           cohort_summary_stats_table = cohortTableNames$cohortSummaryStatsTable,
-                           cohort_censor_stats_table = cohortTableNames$cohortCensorStatsTable,                           
-                           warnOnMissingParameters = TRUE)
-  sql <- SqlRender::translate(sql = sql, targetDialect = connection@dbms)
-  DatabaseConnector::executeSql(connection, sql, progressBar = FALSE, reportOverallTime = FALSE)
   
-  logCreateTableMessage <- function(schema, tableName) {
-    ParallelLogger::logInfo("- Created table ", schema, ".", tableName)
+  # Create a list of the tables to create and determine which require creation
+  # if we are incremental mode
+  createTableFlagList <- lapply(cohortTableNames, FUN=function(x) { x = TRUE })
+  if (incremental) {
+    tables <- DatabaseConnector::getTableNames(connection, cohortDatabaseSchema)
+    for (i in 1:length(cohortTableNames)) {
+      if (toupper(cohortTableNames[i]) %in% toupper(tables)) {
+        createTableFlagList[i] <- FALSE
+        ParallelLogger::logInfo("Table \"", cohortTableNames[i], "\" already exists and in incremental mode, so not recreating it.")
+      }
+    }
   }
-  mapply(logCreateTableMessage, schema=cohortDatabaseSchema, tableName=cohortTableNames)
+  
+  if (any(unlist(createTableFlagList, use.names = FALSE))) {
+    ParallelLogger::logInfo("Creating cohort tables")
+    sql <- SqlRender::readSql(system.file("sql/sql_server/CreateCohortTables.sql", package = "CohortGenerator", mustWork = TRUE))
+    sql <- SqlRender::render(sql = sql, 
+                             cohort_database_schema = cohortDatabaseSchema,
+                             create_cohort_table = createTableFlagList$cohortTable,
+                             create_cohort_inclusion_table = createTableFlagList$cohortInclusionTable,
+                             create_cohort_inclusion_result_table = createTableFlagList$cohortInclusionResultTable,
+                             create_cohort_inclusion_stats_table = createTableFlagList$cohortInclusionStatsTable,
+                             create_cohort_summary_stats_table = createTableFlagList$cohortSummaryStatsTable,
+                             create_cohort_censor_stats_table = createTableFlagList$cohortCensorStatsTable,
+                             cohort_table = cohortTableNames$cohortTable,
+                             cohort_inclusion_table = cohortTableNames$cohortInclusionTable,
+                             cohort_inclusion_result_table = cohortTableNames$cohortInclusionResultTable,
+                             cohort_inclusion_stats_table = cohortTableNames$cohortInclusionStatsTable,
+                             cohort_summary_stats_table = cohortTableNames$cohortSummaryStatsTable,
+                             cohort_censor_stats_table = cohortTableNames$cohortCensorStatsTable,
+                             warnOnMissingParameters = TRUE)
+    sql <- SqlRender::translate(sql = sql, targetDialect = connection@dbms)
+    DatabaseConnector::executeSql(connection, sql, progressBar = FALSE, reportOverallTime = FALSE)
+    
+    logCreateTableMessage <- function(schema, tableName) {
+      ParallelLogger::logInfo("- Created table ", schema, ".", tableName)
+    }
+    for (i in 1:length(createTableFlagList)) {
+      if (createTableFlagList[[i]]) {
+        logCreateTableMessage(schema=cohortDatabaseSchema, tableName=cohortTableNames[i])
+      }
+    }
 
-  delta <- Sys.time() - start
-  ParallelLogger::logInfo("Creating cohort tables took ", round(delta, 2), attr(delta, "units"))
+    delta <- Sys.time() - start
+    ParallelLogger::logInfo("Creating cohort tables took ", round(delta, 2), attr(delta, "units"))
+  }
 }
--- tests/testthat/test-CohortTables.R
+++ tests/testthat/test-CohortTables.R
@@ -40,7 +40,7 @@ test_that("Create cohort tables with connectionDetails", {
   )
 })
 
-test_that("Create cohort table with connection", {
+test_that("Create cohort tables with connection", {
   conn = DatabaseConnector::connect(connectionDetails = connectionDetails)
   cohortTableNames <- getCohortTableNames(cohortTable = "a",
                                           cohortInclusionTable = "b",
@@ -51,10 +51,88 @@ test_that("Create cohort table with connection", {
   createCohortTables(connectionDetails = connectionDetails,
                      cohortDatabaseSchema = "main",
                      cohortTableNames = cohortTableNames)
-  
+
   for(i in names(cohortTableNames)) {
     sql <- paste("SELECT * FROM", cohortTableNames[[i]], ";")
     results <- DatabaseConnector::querySql(conn, sql = sql)
     expect_equal(nrow(results), 0)
   }
-  DatabaseConnector::disconnect(conn)})
+  DatabaseConnector::disconnect(conn)
+})
+
+test_that("Create cohort tables with incremental = TRUE", {
+  conn = DatabaseConnector::connect(connectionDetails = connectionDetails)
+  cohortTableNames <- getCohortTableNames(cohortTable = "incrementalTrue")
+  # Call the 1st time and verify the tables exist
+  createCohortTables(connectionDetails = connectionDetails,
+                     cohortDatabaseSchema = "main",
+                     cohortTableNames = cohortTableNames,
+                     incremental = TRUE)
+
+  for(i in names(cohortTableNames)) {
+    sql <- paste("SELECT * FROM", cohortTableNames[[i]], ";")
+    results <- DatabaseConnector::querySql(conn, sql = sql)
+    expect_equal(nrow(results), 0)
+  }
+
+  # Call again and verify the table creation is skipped
+  expect_invisible(
+    createCohortTables(connectionDetails = connectionDetails,
+                       cohortDatabaseSchema = "main",
+                       cohortTableNames = cohortTableNames,
+                       incremental = TRUE)
+  )
+
+  DatabaseConnector::disconnect(conn)
+})
+
+test_that("Create cohort tables with incremental = TRUE and partial table creation works", {
+  conn = DatabaseConnector::connect(connectionDetails = connectionDetails)
+  cohortTableNames <- getCohortTableNames(cohortTable = "incrementalPartial")
+  
+  # Create only a cohort table
+  sql <- "IF OBJECT_ID('@cohort_database_schema.@cohort_table', 'U') IS NOT NULL
+          DROP TABLE @cohort_database_schema.@cohort_table;
+  
+          CREATE TABLE @cohort_database_schema.@cohort_table (
+            cohort_definition_id BIGINT,
+            subject_id BIGINT,
+            cohort_start_date DATE,
+            cohort_end_date DATE
+          );
+          
+          INSERT INTO @cohort_database_schema.@cohort_table (
+            cohort_definition_id,
+            subject_id,
+            cohort_start_date,
+            cohort_end_date
+          )
+          SELECT
+            1,1,1.0,1.0
+          ;
+  "
+  sql <- SqlRender::render(sql = sql, cohort_database_schema = "main", cohort_table = cohortTableNames$cohortTable)
+  sql <- SqlRender::translate(sql = sql, targetDialect = connectionDetails$dbms)
+  DatabaseConnector::executeSql(connection = conn, sql = sql, progressBar = FALSE, reportOverallTime = FALSE)
+
+  # Verify the table exists and contains a record
+  sql <- paste0("SELECT * FROM main.", cohortTableNames$cohortTable, ";")
+  results <- DatabaseConnector::querySql(conn, sql = sql)
+  expect_equal(nrow(results), 1)
+  
+  # Create the cohort tables and verify
+  createCohortTables(connectionDetails = connectionDetails,
+                     cohortDatabaseSchema = "main",
+                     cohortTableNames = cohortTableNames,
+                     incremental = TRUE)
+  
+  for(i in names(cohortTableNames)) {
+    sql <- paste("SELECT * FROM", cohortTableNames[[i]], ";")
+    results <- DatabaseConnector::querySql(conn, sql = sql)
+    expectedRowCount <- ifelse(cohortTableNames[[i]] == cohortTableNames$cohortTable, 1, 0)
+    expect_equal(nrow(results), expectedRowCount)
+  }
+  
+  DatabaseConnector::disconnect(conn)
+})
+
