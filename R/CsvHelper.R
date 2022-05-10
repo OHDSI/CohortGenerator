e2a869675afc928059517471f4a1099f49e65b6a
Anthony Sena <asena5@its.jnj.com>
2022-05-10 18:06:50 -0400

    Add rule enforcement warnings

--- R/CsvHelper.R
+++ R/CsvHelper.R
@@ -37,20 +37,75 @@ readCsv <- function(file) {
 #'
 #' @description
 #' This function is used to centralize the function for writing
-#' CSV files across the HADES ecosystem.
+#' CSV files across the HADES ecosystem. This function may also raise warnings
+#' if the data is stored in a format that will not work with the HADES standard
+#' for uploading to a results database.
 #' 
 #' @param x  A data frame or tibble to write to disk.
 #' 
 #' @param file  The csv file to write.
-#'
+#' 
+#' @param warnOnUploadRuleViolations When TRUE, this function will provide
+#' warning messages that may indicate if the data is stored in a format in the
+#' CSV that may cause problems when uploading to a database.
+#' 
 #' @return 
 #'  Returns the input x invisibly.
 #'
 #' @export
-writeCsv <- function(x, file) {
+writeCsv <- function(x, file, warnOnUploadRuleViolations = TRUE) {
+  if (warnOnUploadRuleViolations) {
+    # Check the data.frame to ensure it is in the proper format for upload
+    isFormattedForDatabaseUpload(x)
+    
+    # Check if the file name is in lower snake case format
+    fileName <- gsub(x = basename(file), pattern = ".csv", replacement = "")
+    if (!isSnakeCase(fileName)) {
+      warning(paste("The filename:", basename(file), "is not in snake case format"))
+    }
+    
+    # Also perform a check to see if the fileName end in "s" which might indicate
+    # that the resulting file name is plural
+    if (endsWith(fileName, "s")) {
+      warning(paste("The filename:", basename(file), "may be plural since it ends in 's'. Please ensure you are using singular nouns for your file names."))
+    }
+  }
+  
+  # Write the file
   readr::write_csv(x = x,
                    file = file)
   invisible(x)
+}
+
+
+#' Is the data.frame formatted for uploading to a database?
+#'
+#' @description
+#' This function is used to check a data.frame to ensure all 
+#' column names are in snake case format. 
+#' 
+#' @param x  A data frame
+#' @param warn When TRUE, display a warning of any columns are not in snake 
+#' case format
+#' 
+#' @return 
+#' Returns TRUE if all columns are snake case format. If warn == TRUE,
+#' the function will emit a warning on the column names that are not in snake 
+#' case format.
+#'
+#' @export
+isFormattedForDatabaseUpload <- function(x, warn = TRUE) {
+  checkmate::assert_data_frame(x)
+  columnNames <- colnames(x)
+  columnNamesInSnakeCaseFormat <- isSnakeCase(columnNames)
+  if (!all(columnNamesInSnakeCaseFormat) && warn) {
+    problemColumns <- columnNames[-columnNamesInSnakeCaseFormat]
+    problemColumnsFixed <- paste(snakecase::to_snake_case(problemColumns), collapse = ", ")
+    problemColumnsWarning <- paste(problemColumns, collapse = ", ")
+    problemColumnsFixedWarning <- paste(problemColumnsFixed, collapse = ", ")
+    warning(paste("The following data.frame column names are not in snake case format:", problemColumnsWarning, "\n  Consider revising the column names to:", problemColumnsFixedWarning))
+  }
+  return(all(columnNamesInSnakeCaseFormat))
 }
 
 #' Used to check if a string is in lower camel case
--- tests/testthat/test-CsvHelper.R
+++ tests/testthat/test-CsvHelper.R
@@ -1,6 +1,7 @@
 library(testthat)
 library(CohortGenerator)
 
+# isCamelCase Tests ---------------
 test_that("isCamelCase returns TRUE", {
   expect_true(isCamelCase("camelCaseTest"))
 })
@@ -17,6 +18,7 @@ test_that("isCamelCase returns FALSE with leading capital", {
   expect_false(isCamelCase("CamelCaseTest"))
 })
 
+# isSnakeCase Tests ---------------
 test_that("isSnakeCase returns TRUE", {
   expect_true(isSnakeCase("snake_case_test"))
 })
@@ -32,3 +34,21 @@ test_that("isSnakeCase returns FALSE with double underscore", {
 test_that("isSnakeCase returns FALSE with upper case character", {
   expect_false(isSnakeCase("snake_Case_test"))
 })
+
+# isFormattedForDatabaseUpload Tests ---------------
+test_that("isFormattedForDatabaseUpload returns TRUE with properly formatted data frame", {
+  df <- createEmptyCohortDefinitionSet()
+  names(df) <- SqlRender::camelCaseToSnakeCase(names(df))
+  expect_true(isFormattedForDatabaseUpload(df, warn = TRUE))
+})
+
+test_that("isFormattedForDatabaseUpload returns FALSE with improperly formatted data frame", {
+  df <- createEmptyCohortDefinitionSet()
+  expect_false(isFormattedForDatabaseUpload(df, warn = FALSE))
+})
+
+test_that("isFormattedForDatabaseUpload provides warning with improperly formatted data frame", {
+  df <- createEmptyCohortDefinitionSet()
+  expect_warning(isFormattedForDatabaseUpload(df, warn = TRUE))
+})
+
