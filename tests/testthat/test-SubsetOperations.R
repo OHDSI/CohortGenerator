e5213efbea677c1f06703f3a66b876aca120d8f3
Jamie Gilbert <JGilber2@its.jnj.com>
2023-03-08 11:01:49 -0800

    Subset naming conventions updated

--- tests/testthat/test-SubsetOperations.R
+++ tests/testthat/test-SubsetOperations.R
@@ -4,7 +4,7 @@ test_that("Cohort subset naming and instantitation", {
                                           negate = FALSE,
                                           startWindow = createSubsetCohortWindow(0, 90, "cohortStart"),
                                           endWindow = createSubsetCohortWindow(0, 50, "cohortEnd"))
-  expectedName <- "Coh: {subjects in all of cohorts: (11, 22) Starting D: 0 and D: 90 of target cohort start and Ending D: 0 and D: 50 of target cohort end}"
+  expectedName <- "in all of cohorts: (11, 22) starts within D: 0 - D: 90 of cohort start and ends D: 0 - D: 50 of cohort end"
   expect_equal(expectedName, cohortSubsetNamed$name)
 
   cohortSubsetNamed$name <- "foo"
@@ -15,7 +15,7 @@ test_that("Cohort subset naming and instantitation", {
                                           negate = TRUE,
                                           startWindow = createSubsetCohortWindow(0, 90, "cohortStart"),
                                           endWindow = createSubsetCohortWindow(0, 50, "cohortEnd"))
-  expectedName <- "Coh: {subjects not in any of cohorts: (11, 22) Starting D: 0 and D: 90 of target cohort start and Ending D: 0 and D: 50 of target cohort end}"
+  expectedName <-  "not in any of cohorts: (11, 22) starts within D: 0 - D: 90 of cohort start and ends D: 0 - D: 50 of cohort end"
   expect_equal(expectedName, cohortSubsetNamed$name)
 })
 
@@ -30,48 +30,52 @@ test_that("limit subset naming and instantitation", {
   expect_error(createLimitSubset(calendarEndDate = "2001/01/01", priorTime = 300, limitTo = "pirstEmaining"))
 
   limitSubsetNamed <- createLimitSubset(priorTime = 365, followUpTime = 0, limitTo = "latestRemaining")
-  expectedName <- "Limit to: {latest remaining occurence with at least 365 days prior observation}"
+  expectedName <- "latest remaining occurence with at least 365 days prior observation"
   expect_equal(expectedName, limitSubsetNamed$name)
   limitSubsetNamed$name <- "foo"
   expect_equal("foo", limitSubsetNamed$name)
 
   limitSubsetNamed <- createLimitSubset(priorTime = 365, followUpTime = 2, limitTo = "firstEver")
-  expectedName <- "Limit to: {first ever occurence with at least 365 days prior observation and 2 days follow up observation}"
+  expectedName <- "first ever occurence with at least 365 days prior observation and 2 days follow up observation"
   expect_equal(expectedName, limitSubsetNamed$name)
 
   limitSubsetNamed <- createLimitSubset(priorTime = 0, followUpTime = 200, limitTo = "lastEver")
-  expectedName <- "Limit to: {last ever occurence with at least 200 days follow up observation}"
+  expectedName <- "last ever occurence with at least 200 days follow up observation"
   expect_equal(expectedName, limitSubsetNamed$name)
 
   limitSubsetNamed <- createLimitSubset(priorTime = 0, followUpTime = 200, limitTo = "earliestRemaining")
-  expectedName <- "Limit to: {earliest remaining occurence with at least 200 days follow up observation}"
+  expectedName <- "earliest remaining occurence with at least 200 days follow up observation"
   expect_equal(expectedName, limitSubsetNamed$name)
 
   limitSubsetNamed <- createLimitSubset(calendarStartDate = "2001/01/01")
-  expectedName <- "Limit to: {occurs after 2001-01-01}"
+  expectedName <- "occurs after 2001-01-01"
   expect_equal(expectedName, limitSubsetNamed$name)
   
   limitSubsetNamed <- createLimitSubset(calendarEndDate = "2001/01/01")
-  expectedName <- "Limit to: {occurs before 2001-01-01}"
+  expectedName <- "occurs before 2001-01-01"
   expect_equal(expectedName, limitSubsetNamed$name)
   
   limitSubsetNamed <- createLimitSubset(calendarStartDate = "2001/12/31", calendarEndDate = "2010/01/01")
-  expectedName <- "Limit to: {occurs after 2001-12-31 and before 2010-01-01}"
+  expectedName <- "occurs after 2001-12-31 and before 2010-01-01"
   expect_equal(expectedName, limitSubsetNamed$name)
 })
 
 
 test_that("Demographic subset naming", {
-  demoSubset <- createDemographicSubset(ageMin = 18, ageMax = 99)
-  expectedName <- "Demo: {age >= 18 and age <= 99}"
+
+  demoSubset <- createDemographicSubset(ageMin = 32)
+  expectedName <- "aged 32+"
+  
+  demoSubset <- createDemographicSubset(gender= "male", ageMin = 18, ageMax = 99)
+  expectedName <- "males aged 18 - 99"
   expect_equal(expectedName, demoSubset$name)
   
   demoSubset <- createDemographicSubset(ageMin = 18, ageMax = 99, gender = c("foo", "MAlE", "female"), race = c(44), ethnicity = c(88))
-  expectedName <- "Demo: {age >= 18 and age <= 99 and Gender concept 0, 8507, 8532 and Race concept 44 and Ethnicity concept 88}"
+  expectedName <- "unknown gender, males, females aged 18 - 99, race: 44, ethnicity: 88"
   expect_equal(expectedName, demoSubset$name)
   
   demoSubset <- createDemographicSubset(ageMin = 18, ageMax = 99, gender = c(11, 8532), race = c(44), ethnicity = c(88))
-  expectedName <- "Demo: {age >= 18 and age <= 99 and Gender concept 11, 8532 and Race concept 44 and Ethnicity concept 88}"
+  expectedName <- "gender concept: 11, females aged 18 - 99, race: 44, ethnicity: 88"
   expect_equal(expectedName, demoSubset$name)
   
   demoSubset$name <- "foo"
