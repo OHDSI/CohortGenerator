2be738866774f424339d4aded4c7e8ae52a5911f
Anthony Sena <asena5@its.jnj.com>
2023-04-07 12:52:41 -0400

    Set folder configuration before calling Eunomia to get connection details

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,14 +1,24 @@
 dbmsPlatforms <- c("redshift", "postgresql", "oracle", "sql server")
-connectionDetails <- Eunomia::getEunomiaConnectionDetails()
+# Set the data folders before calling Eunomia::getEunomiaConnectionDetails()
 outputFolder <- tempfile()
-dir.create(outputFolder)
+dir.create(outputFolder, recursive = T, showWarnings = F)
 dataFolder <- file.path(outputFolder, "eunomia_data")
-dir.create(dataFolder)
+dir.create(dataFolder, recursive = T, showWarnings = F)
 oldEunomiaFolder <- Sys.getenv("EUNOMIA_DATA_FOLDER")
 Sys.setenv("EUNOMIA_DATA_FOLDER" = dataFolder)
+eunomiaDbFile <- file.path(outputFolder, "data", "testEunomia.sqlite")
+if (!dir.exists(file.path(outputFolder, "data"))) {
+  dir.create(file.path(outputFolder, "data"), recursive = T, showWarnings = F)
+}
+connectionDetails <- Eunomia::getEunomiaConnectionDetails(
+  databaseFile = eunomiaDbFile
+)
+
+connectionDetails <- Eunomia::getEunomiaConnectionDetails()
 withr::defer(
   {
-    unlink(outputFolder)
+    unlink(outputFolder, recursive = TRUE, force = TRUE)
+    unlink(eunomiaDbFile, recursive = TRUE, force = TRUE)
     Sys.setenv("EUNOMIA_DATA_FOLDER" = oldEunomiaFolder)
   },
   testthat::teardown_env()
