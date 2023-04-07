e651ff4acfe0244636608ce8dbc0eef1ee2e1a0b
Anthony Sena <asena5@its.jnj.com>
2023-04-07 11:27:56 -0400

    Set EUNOMIA_DATA_FOLDER property

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,9 +2,14 @@ dbmsPlatforms <- c("redshift", "postgresql", "oracle", "sql server")
 connectionDetails <- Eunomia::getEunomiaConnectionDetails()
 outputFolder <- tempfile()
 dir.create(outputFolder)
+dataFolder <- file.path(outputFolder, "eunomia_data")
+dir.create(dataFolder)
+oldEunomiaFolder <- Sys.getenv("EUNOMIA_DATA_FOLDER")
+Sys.setenv("EUNOMIA_DATA_FOLDER" = dataFolder)
 withr::defer(
   {
     unlink(outputFolder)
+    Sys.setenv("EUNOMIA_DATA_FOLDER" = oldEunomiaFolder)
   },
   testthat::teardown_env()
 )
