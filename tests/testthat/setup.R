fbb97b29b3e9c2fa7f1191f1e3044722e8cb5de9
Anthony Sena <asena5@its.jnj.com>
2023-04-07 13:06:44 -0400

    Update to getting Eunomia connection

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -6,19 +6,14 @@ dataFolder <- file.path(outputFolder, "eunomia_data")
 dir.create(dataFolder, recursive = T, showWarnings = F)
 oldEunomiaFolder <- Sys.getenv("EUNOMIA_DATA_FOLDER")
 Sys.setenv("EUNOMIA_DATA_FOLDER" = dataFolder)
-eunomiaDbFile <- file.path(outputFolder, "data", "testEunomia.sqlite")
-if (!dir.exists(file.path(outputFolder, "data"))) {
-  dir.create(file.path(outputFolder, "data"), recursive = T, showWarnings = F)
-}
-connectionDetails <- Eunomia::getEunomiaConnectionDetails(
-  databaseFile = eunomiaDbFile
-)
 
+# Get the Eunomia connection details
 connectionDetails <- Eunomia::getEunomiaConnectionDetails()
+
+# Cleanup after testing
 withr::defer(
   {
     unlink(outputFolder, recursive = TRUE, force = TRUE)
-    unlink(eunomiaDbFile, recursive = TRUE, force = TRUE)
     Sys.setenv("EUNOMIA_DATA_FOLDER" = oldEunomiaFolder)
   },
   testthat::teardown_env()
