393c5bb873b248941f82789a9b2622aa574d1fd7
Anthony Sena <asena5@its.jnj.com>
2024-06-13 15:50:48 -0400

    Fix comment

--- R/ResultsDataModel.R
+++ R/ResultsDataModel.R
@@ -1,6 +1,6 @@
 # Copyright 2024 Observational Health Data Sciences and Informatics
 #
-# This file is part of SelfControlledCaseSeries
+# This file is part of CohortGenerator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
