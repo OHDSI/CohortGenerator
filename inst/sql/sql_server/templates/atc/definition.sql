e4d901be8651a6a35f02f1ff4bae119f26c3132e
jgilber2 <jgilber2@its.jnj.com>
2025-05-13 14:29:04 -0700

    fix for definition of non-era merged atc cohorts

--- inst/sql/sql_server/templates/atc/definition.sql
+++ inst/sql/sql_server/templates/atc/definition.sql
@@ -101,8 +101,7 @@ select
   , cohort_end_date
 from (
 select
-  et.cohort_definition_id
-  , de1.concept_name
+  , de1.concept_id
   , de1.person_id
   , de1.cohort_start_date
   , de1.cohort_end_date
