fc51a000a1fd8f9258854f1432e88c21d5385518
Anthony Sena <asena5@its.jnj.com>
2025-05-30 11:00:46 -0400

    Revise migration to copy/drop table

--- inst/sql/sql_server/migrations/Migration_2-v0.12.0.sql
+++ inst/sql/sql_server/migrations/Migration_2-v0.12.0.sql
@@ -1,4 +1,35 @@
 -- Database migrations for version 0.12.0
 -- Adds missing database_id to cg_cohort_censor_stats primary key
-ALTER TABLE @database_schema.@table_prefixcg_cohort_censor_stats DROP CONSTRAINT cg_cohort_censor_stats_pkey;
-ALTER TABLE @database_schema.@table_prefixcg_cohort_censor_stats ADD CONSTRAINT cg_cohort_censor_stats_pkey PRIMARY KEY (cohort_definition_id, lost_count, database_id);
+DROP TABLE IF EXISTS @database_schema.@table_prefixcg_cohort_censor_stats_temp;
+
+CREATE TABLE @database_schema.@table_prefixcg_cohort_censor_stats_temp (
+	 cohort_definition_id BIGINT NOT NULL,
+	 lost_count BIGINT NOT NULL,
+	 database_id VARCHAR
+)
+
+INSERT INTO @database_schema.@table_prefixcg_cohort_censor_stats_temp
+SELECT
+  cohort_definition_id,
+  lost_count,
+  database_id
+FROM @database_schema.@table_prefixcg_cohort_censor_stats
+;
+
+DROP TABLE @database_schema.@table_prefixcg_cohort_censor_stats;
+CREATE TABLE @database_schema.@table_prefixcg_cohort_censor_stats_temp (
+	 cohort_definition_id BIGINT NOT NULL,
+	 lost_count BIGINT NOT NULL,
+	 database_id VARCHAR,
+	 PRIMARY KEY(cohort_definition_id,lost_count,database_id)
+)
+
+INSERT INTO @database_schema.@table_prefixcg_cohort_censor_stats
+SELECT
+  cohort_definition_id,
+  lost_count,
+  database_id
+FROM @database_schema.@table_prefixcg_cohort_censor_stats_temp
+;
+
+DROP TABLE @database_schema.@table_prefixcg_cohort_censor_stats_temp;
