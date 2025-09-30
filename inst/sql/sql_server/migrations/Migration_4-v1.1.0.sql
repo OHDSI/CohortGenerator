f0ed383ca696cc8dd241a2177d0f55ed13bec023
jgilber2 <jgilber2@its.jnj.com>
2025-09-30 14:09:30 -0700

    Removed template migrations

--- inst/sql/sql_server/migrations/Migration_4-v1.1.0.sql
+++ inst/sql/sql_server/migrations/Migration_4-v1.1.0.sql
@@ -1,11 +1,10 @@
--- Database migrations for version 1.0.0
+-- Database migrations for version 1.1.0
 -- Adds concept set tables to schema
--- Adds is template field to cohort definition table
+
 {DEFAULT @cg_concept_set = cg_concept_set}
 {DEFAULT @cg_concept_set = cg_concept_set}
 {DEFAULT @cg_cohort_concept_set = cg_cohort_concept_set}
-{DEFAULT @cg_cohort_definition = cg_cohort_definition}
-{DEFAULT @cg_cohort_template_definition = cg_cohort_template_definition}
+
 
 CREATE TABLE @database_schema.@table_prefix@cg_concept_set(
     concept_set_id varchar NOT NULL,
@@ -26,13 +25,4 @@ CREATE TABLE @database_schema.@table_prefix@cg_cohort_concept_set(
     concept_set_id varchar NOT NULL,
     cohort_definition_id bigint NOT NULL,
     primary key(concept_set_id, cohort_definition_id)
-);
-
-ALTER TABLE @database_schema.@table_prefix@cg_cohort_definition ADD COLUMN is_templated_cohort INT;
-
-
-CREATE TABLE @database_schema.@table_prefix@cg_cohort_template_definition (
-    template_definition_id BIGINT,
-    json varchar,
-    PRIMARY KEY(template_definition_id)
 );
