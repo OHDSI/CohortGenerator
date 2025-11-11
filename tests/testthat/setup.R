6b4e9fa5172ccb688b7d80ef53a2feb7d0f33124
Anthony Sena <asena5@its.jnj.com>
2025-11-11 09:40:14 -0500

    Bump github-pagest-deploy-action and debug HADES R CMD check failure by disabling GBQ

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,4 +1,4 @@
-dbmsPlatforms <- c("bigquery", "oracle", "postgresql", "redshift", "spark", "sql server") # DISABLE "snowflake" test for now
+dbmsPlatforms <- c("oracle", "postgresql", "redshift", "spark", "sql server") # DISABLE "bigquery", "snowflake" tests for now
 connectionDetails <- Eunomia::getEunomiaConnectionDetails()
 outputFolder <- tempfile()
 dir.create(outputFolder)
