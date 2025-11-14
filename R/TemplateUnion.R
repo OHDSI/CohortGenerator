8031ccd93b42a26f9cd3398a2ce3437e26654e9b
jgilber2 <jgilber2@its.jnj.com>
2025-11-14 09:40:06 -0800

    fix for union cohort templates

--- R/TemplateUnion.R
+++ R/TemplateUnion.R
@@ -78,7 +78,7 @@ createUnionCohortTemplate <- function(cohortIds,
                 ELSE 0
               END
             ) OVER (PARTITION BY c.subject_id ORDER BY c.cohort_start_date) AS group_id
-        FROM @cohort_database_scheme.@cohort_table c
+        FROM @cohort_database_schema.@cohort_table c
         WHERE c.cohort_definition_id IN (@cohort_ids)
     )
     GROUP BY subject_id group_id
