0cde17850a777934f000f82aafe1cb6e90936af2
Anthony Sena <asena5@its.jnj.com>
2026-07-24 12:02:26 -0400

    Consolidate test files

--- tests/testthat/test-ResultsDataModel.R
+++ tests/testthat/test-ResultsDataModel.R
@@ -186,3 +186,17 @@ test_that("Create schema and upload on Sqlite", {
 
   on.exit(unlink(sqliteInfo$connectionDetails$server(), force = TRUE), add = TRUE)
 })
+
+test_that("resultsDataModelSpecification.csv has no parsing problems", {
+  file <- system.file("csv", "resultsDataModelSpecification.csv", package = "CohortGenerator")
+
+  dat <- readr::read_csv(
+    file = file,
+    col_types = readr::cols(),
+    lazy = FALSE,
+    progress = FALSE,
+    show_col_types = FALSE
+  )
+
+  expect_equal(nrow(vroom::problems(dat)), 0)
+})
